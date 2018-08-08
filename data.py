b3aa489796afb18953668bb0365d0ccc806fc7d5
Ksevegan <41348288+Ksevegan@users.noreply.github.com>
2018-08-07 17:57:01 -0700

    Add files via upload
    How to use data

---
+++ data.py
@@ -0,0 +1,18 @@
+life = {
+'Kathy': 15,
+'John': 19,
+'Raymond': 20,
+'Elena': 16,
+'Rodrigo': 18
+
+}
+
+print (life)
+
+empty = {}
+
+username = input("What's your name?")
+age = input("How old are you?")
+
+life [username] = age
+print (life)
