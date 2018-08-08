fa4d2057ea4c38554730fa89c4ca0e16f0cf9da4
Ksevegan <41348288+Ksevegan@users.noreply.github.com>
2018-08-07 17:52:37 -0700

    Add files via upload
    How to use 2 list and randomly produce a name.

---
+++ NameGen.py
@@ -0,0 +1,4 @@
+import random
+First = ("Jessica", "Cindy", "Jason", "Andrew", "Lauren", "Rowan","Gabriel", "Shayla", "Angel", "Max")
+Last = ("Garcia", "Velasco", "Brown", "Hernandez", "Ramirez", "Nguyen", "Gonzalez", "Smith", "Queen", "Escobar")
+print (random.choice(First)  +  random.choice(Last))
